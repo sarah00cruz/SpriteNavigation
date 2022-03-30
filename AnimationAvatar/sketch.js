45e7a386636558aa3876166e3640a1928aeff0fb
Sarah Cruz <sdcruz@dons.usfca.edu>
2022-03-30 10:02:14 -0700

    Add files via upload

---
+++ AnimationAvatar/sketch.js
@@ -0,0 +1,133 @@
+/***********************************************************************************
+  Sprite Navigation
+
+  Simple use of the p5.play library
+------------------------------------------------------------------------------------
+	To use:
+	Add this line to the index.html
+
+  <script src="p5.timer.js"></script>
+***********************************************************************************/
+
+// This is a 'sprite' which we can move
+var ghost, ghostS;
+var speed = 10;
+
+// The is a static sprite
+var star;
+var starImg;
+
+// keycods for W-A-S-D
+const W_KEY = 87;
+const S_KEY = 83;
+const D_KEY = 68;
+const A_KEY = 65;
+avatars[2] = new Avatar("Sarah", 300, 150, 'assets/ghost1.png', 'assets/ghost3.png');
+
+function preload() {
+  starImg = loadImage('assets/fullStar.png');
+  ghostImg = loadImage('assets/ghost1.png');
+}
+// Setup code goes here
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // create a sprite with dimensions
+  ghost = createSprite(400, 150);
+  ghostS = createSprite(300 , 200);
+
+  // This is a *numbered* sequence of PNG files
+  // We add animation to different sprites
+  ghost.addAnimation('floating', 'assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  ghostS.addAnimation('floating', 'assets/ghost1.png', 'assets/ghost3.png');
+
+  // create a star in the middle of the screen
+  //star = createSprite(width/2, height/2);
+  //star.addImage('star', starImg);
+
+  frameRate(30);
+ }
+
+// Draw code goes here
+function draw() {
+  // could draw a PNG file here
+  background(255);
+
+  // trap keyboard arrow keys
+  checkMovement();
+
+  // drawSprites is a function in p5.play, draws all the sprites
+  drawSprites();
+
+  // callback function
+  ghost.overlap(ghostS, ghostCollision);
+  //ghostS.overlap(star, ghostCollision);
+
+}
+
+// This will reset position
+function keyPressed() {
+  if( key === ' ') {
+    ghost.position.x = width/2;
+    ghost.position.y = height/2;
+    ghostS.position.x = width/2;
+    ghostS.position.y = height/2;
+  }
+  // if(key === ' '){
+  //   ghostS.position.x = width/2;
+  //   ghostS.position.y = height/2;
+  // }
+}
+
+function checkMovement() {
+  // Check x movement
+  if(keyIsDown(68)) {
+    ghost.velocity.x = speed;
+    ghostS.velocity.x = speed;
+
+  }
+  else if(keyIsDown(65)) {
+    ghost.velocity.x = -speed;
+    ghostS.velocity.x = -speed;
+
+  }
+  else {
+    ghost.velocity.x = 0;
+    ghostS.velocity.x = 0;
+
+  }
+
+  // Check y movement
+  if(keyIsDown(83)) {
+    ghost.velocity.y = speed;
+    ghostS.velocity.y = speed;
+
+  }
+  else if(keyIsDown(87)) {
+    ghost.velocity.y = -speed;
+    ghostS.velocity.y = -speed;
+
+  }
+  else {
+    ghost.velocity.y = 0;
+    ghostS.velocity.y = 0;
+
+  }
+
+  // use keyIsDown(W_KEY)  for your player sprite, moving up
+  // use keyIsDown(A_KEY)  for your player sprite, moving left
+  // ...etc
+  
+
+}
+
+// SpriteA is the sprite in question, spriteA will be ghost in this case
+// SpriteB is the one that it collided with
+function ghostCollision(spriteA, spriteB) {
+  ghost.position.x = 100;
+  ghost.position.y = 100;
+  // ghostS.position.x = 100;
+  // ghostS.position.y = 100;
+
+  //spriteB.remove();
+}
